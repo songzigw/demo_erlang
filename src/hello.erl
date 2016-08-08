88d5a0f30f3c8e699e4483d0ccc99c9f7d1ba6dd
songzigw <songzigw@163.com>
2016-08-08 20:08:46 +0800

    hello

--- src/hello.erl
+++ src/hello.erl
@@ -1,11 +1,8 @@
 -module(hello).
+
 -export([test/0]).
 
 test()->
     List = [1,2,3,4,5,6],
-    Lidy = lists:filter(fun(H) ->
-                                if H == 5 -> true;
-                                    true  -> false
-                                end
-                        end, List),
-    io:format("~p~n", [Lidy]).
+    io:format("~p~n", [List]).
+
